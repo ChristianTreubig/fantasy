5fe842b9d35c6a4fac390a1b9615ccff996ce5b2
ChristianTreubig <christian.treubig@gmail.com>
2015-08-08 02:17:45 -0400

    move values/proportions functions from controller to service

--- football/draft/DraftController.js
+++ football/draft/DraftController.js
@@ -26,64 +26,8 @@ football_app.controller('DraftController', ['$scope', "football", "footballConfi
             return player.Owner === "me";
         }
         
-        //Get proportion of predraft values allocated to each position:
-        var calculatePositionProportions = function() {
-            QB_total = 0; //Need to put these totals in $scope?
-            RB_total = 0;
-            WR_total = 0;
-            TE_total = 0;
-            K_total = 0;
-            angular.forEach($scope.players, function(value, index){
-                if (value.Pos == "QB") {
-                    QB_total += value.AverageAuction;
-                }
-                else if (value.Pos == "RB") {
-                    RB_total += value.AverageAuction;
-                }
-                else if (value.Pos == "WR") {
-                    WR_total += value.AverageAuction;
-                }
-                else if (value.Pos == "TE") {
-                    TE_total += value.AverageAuction;
-                }
-                else if (value.Pos == "K") {
-                    K_total += value.AverageAuction;
-                }
-            });
-            All_total = QB_total + RB_total + WR_total + TE_total + K_total;
-            $scope.QB_prop = QB_total / All_total;
-            $scope.RB_prop = RB_total / All_total;
-            $scope.WR_prop = WR_total / All_total;
-            $scope.TE_prop = TE_total / All_total;
-            $scope.K_prop = K_total / All_total;
-            //alert(QB_prop + RB_prop + WR_prop + TE_prop);
-        }
-        
-        calculatePositionProportions();
-        //alert($scope.QB_prop);
-        
-        //Get proportion of predraft values for each player in relation to their position:
-        var calculatePlayerProportions = function() {
-            angular.forEach($scope.players, function(value, index){
-                if (value.Pos == "QB") {
-                    $scope.players[index].Prop = value.AverageAuction / QB_total;
-                }
-                else if (value.Pos == "RB") {
-                    $scope.players[index].Prop = value.AverageAuction / RB_total;
-                }
-                else if (value.Pos == "WR") {
-                    $scope.players[index].Prop = value.AverageAuction / WR_total;
-                }
-                else if (value.Pos == "TE") {
-                    $scope.players[index].Prop = value.AverageAuction / TE_total;
-                }
-                else if (value.Pos == "K") {
-                    $scope.players[index].Prop = value.AverageAuction / K_total;
-                }
-            });
-        }
-        
-        calculatePlayerProportions();
+        alert($scope.draftMeta.calculatePositionProportions($scope.players)["RB_prop"]);
+        alert($scope.draftMeta.calculatePlayerProportions($scope.players)[0].Prop);
         
         //Undo previous pick:
         $scope.undo = function() {
--- football/services/football.js
+++ football/services/football.js
@@ -14,9 +14,91 @@ football_app.factory('football', ['$http', function($http) {
     }
 })
 .factory("draftMeta", function() {
+    //Get predraft values allocated to each position:
+    var calculatePositionValues = function (players) {
+        var All_total = 0;
+        var QB_total = 0;
+        var RB_total = 0;
+        var WR_total = 0;
+        var TE_total = 0;
+        var K_total = 0;
+        angular.forEach(players, function(value, index){
+            if (value.Pos == "QB") {
+                QB_total += value.AverageAuction;
+            }
+            else if (value.Pos == "RB") {
+                RB_total += value.AverageAuction;
+            }
+            else if (value.Pos == "WR") {
+                WR_total += value.AverageAuction;
+            }
+            else if (value.Pos == "TE") {
+                TE_total += value.AverageAuction;
+            }
+            else if (value.Pos == "K") {
+                K_total += value.AverageAuction;
+            }
+        });
+        All_total = QB_total + RB_total + WR_total + TE_total + K_total;
+        return {
+            "All_total": All_total,
+            "QB_total": QB_total,
+            "RB_total": RB_total,
+            "WR_total": WR_total,
+            "TE_total": TE_total,
+            "K_total": K_total
+        }
+    }
+    
+    //Get proportion of predraft values allocated to each position:
+    var calculatePositionProportions = function(players) {
+        var QB_total = calculatePositionValues(players)["QB_total"];
+        var RB_total = calculatePositionValues(players)["RB_total"];
+        var WR_total = calculatePositionValues(players)["WR_total"];
+        var TE_total = calculatePositionValues(players)["TE_total"];
+        var K_total = calculatePositionValues(players)["K_total"];
+        var All_total = QB_total + RB_total + WR_total + TE_total + K_total;
+        QB_prop = QB_total / All_total;
+        RB_prop = RB_total / All_total;
+        WR_prop = WR_total / All_total;
+        TE_prop = TE_total / All_total;
+        K_prop = K_total / All_total;
+        return {
+            "QB_prop": QB_prop,
+            "RB_prop": RB_prop,
+            "WR_prop": WR_prop,
+            "TE_prop": TE_prop,
+            "K_prop": K_prop,
+        }
+    }
+    
+    //Get proportion of predraft values for each player in relation to their position:
+    var calculatePlayerProportions = function(players) {
+        angular.forEach(players, function(value, index){
+            if (value.Pos == "QB") {
+                players[index].Prop = value.AverageAuction / calculatePositionValues(players)["QB_total"];
+            }
+            else if (value.Pos == "RB") {
+                players[index].Prop = value.AverageAuction / calculatePositionValues(players)["RB_total"];
+            }
+            else if (value.Pos == "WR") {
+                players[index].Prop = value.AverageAuction / calculatePositionValues(players)["WR_total"];
+            }
+            else if (value.Pos == "TE") {
+                players[index].Prop = value.AverageAuction / calculatePositionValues(players)["TE_total"];
+            }
+            else if (value.Pos == "K") {
+                players[index].Prop = value.AverageAuction / calculatePositionValues(players)["K_total"];
+            }
+        });
+        return players;
+    }
+    
     return{
         playerDollarsRemaining: 200,
         totalDollarsRemaining: null,
-        previousPlayerTaken: null //For undo functionality.
+        previousPlayerTaken: null, //For undo functionality.
+        calculatePositionProportions: calculatePositionProportions,
+        calculatePlayerProportions: calculatePlayerProportions
     }
 });

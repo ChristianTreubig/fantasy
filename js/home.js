e1585a9d368b217f88871588ea1e20a22a9a4606
ChristianTreubig <christian.treubig@gmail.com>
2015-08-19 01:24:21 -0400

    do bulk of markup on home page

--- js/home.js
+++ js/home.js
@@ -1,5 +1,19 @@
-$(window).load(function() {
-    var DELAY = 1000;
+var _throttleTimer = null;
+var _throttleDelay = 50;
+var $window = $(window);
+
+$(document).ready(function() {
+    if (isScrolledIntoView("#football")) {
+            slideLinks();
+    }
+    
+    $window
+        .off('scroll', ScrollHandler)
+        .on('scroll', ScrollHandler);
+});
+
+function slideLinks() {
+    var DELAY = 200;
     var ANIMATION_LENGTH = 500;
     $("#football").delay(DELAY).animate({
         right: '50%'
@@ -13,4 +27,28 @@ $(window).load(function() {
     $("#skating").delay(DELAY + 600).animate({
         left: '50%'
     }, ANIMATION_LENGTH);
-});
+}
+
+function isScrolledIntoView(elem)
+{
+    var $elem = $(elem);
+
+    var docViewTop = $window.scrollTop();
+    var docViewBottom = docViewTop + $window.height();
+
+    var elemTop = $elem.offset().top;
+    var elemBottom = elemTop + $elem.height();
+
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+}
+
+function ScrollHandler(e) {
+    clearTimeout(_throttleTimer);
+    _throttleTimer = setTimeout(function () {
+        
+        if (isScrolledIntoView("#football")) {
+            slideLinks();
+        }
+        
+    }, _throttleDelay);
+}
